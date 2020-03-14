2d0ddbcc410a0e9535cc458e0f07203d3ef8feb4
vib1336 <46282210+vib1336@users.noreply.github.com>
2020-03-15 00:50:19 +0200

    Delete view because it is no longer needed

--- src/Web/CookingPot.Web.ViewModels/Recipes/AllRecipesViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CookingPot.Web.ViewModels.Recipes
-{
-    using System.Collections.Generic;
-
-    public class AllRecipesViewModel
-    {
-        public IEnumerable<DisplayRecipeViewModel> AllRecipes { get; set; }
-    }
-}
